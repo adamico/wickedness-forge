817d9d90857663c924258f48ee241c871b9a310c
D'AMICO Andrea <adam@ladaptlyon.local>
2021-03-16 12:51:01 +0100

    Initial commit

---
+++ src/main/java/kc00l/wickedness/common/item/WickednessDust.java
@@ -0,0 +1,9 @@
+package kc00l.wickedness.common.item;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+public final class WickednessDust extends ForgeRegistryEntry<WickednessDust>
+{
+
+}
---
+++ src/main/java/kc00l/wickedness/common/item/WickednessItems.java
@@ -0,0 +1,10 @@
+package kc00l.wickedness.common.item;
+
+import kc00l.wickedness.common.Wickedness;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public final class WickednessItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Wickedness.MODID);
+}
